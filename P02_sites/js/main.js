38ff36b54cb785325291d13e105c4911f8c2cab8
Jisoo <j.lee.950627@gmail.com>
2020-07-02 00:20:43 +0900

    Made Search page/ Add review
    add review pop up added, searching page created, Responsive review section, Validate the code

--- P02_sites/js/main.js
+++ P02_sites/js/main.js
@@ -1,3 +1,34 @@
+/*  MODAL FOR REVIEW  
+Tutorial from: https://www.w3schools.com/howto/howto_css_modals.asp*/
+
+"use strict";
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("review-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 
 
 var x, i, j, l, ll, selElmnt, a, b, c;
@@ -84,4 +115,3 @@ function closeAllSelect(elmnt) {
 /* If the user clicks anywhere outside the select box,
 then close all select boxes: */
 document.addEventListener("click", closeAllSelect);
-
