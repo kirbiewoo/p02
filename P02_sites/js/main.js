27edfd74f817a0e9886047ed60a12db6e8a27d12
kirby <kirbyw@sfu.ca>
2020-07-01 13:01:34 -0700

    Made changes to About Page + Added image gallery
    issue in image gallery

--- P02_sites/js/main.js
+++ P02_sites/js/main.js
@@ -115,3 +115,42 @@ function closeAllSelect(elmnt) {
 /* If the user clicks anywhere outside the select box,
 then close all select boxes: */
 document.addEventListener("click", closeAllSelect);
+
+
+
+
+/*--tutorial from https://www.w3schools.com/howto/howto_js_slideshow_gallery.asp?
+fbclid=IwAR2CpsrN1zdFV5hoX6G-wHvBOmzKGp7LshUtr2GbrDREGBAIu4PvnyJa6CE*/
+/*-- javascript for the image gallery ---*/
+
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
