c744af1b24cded1a3e2d1c95c25c36cdb86ee3d5
Jisoo <j.lee.950627@gmail.com>
2020-07-04 02:41:42 +0900

    Review/ Size without Javascript
    Made it work without javascript. Tried to do img gallery as well, but it still has some problem.

--- P02_sites/js/main.js
+++ P02_sites/js/main.js
@@ -1,35 +1,3 @@
-/*  MODAL FOR REVIEW  
-Tutorial from: https://www.w3schools.com/howto/howto_css_modals.asp*/
-
-"use strict";
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("review-btn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
 
 var x, i, j, l, ll, selElmnt, a, b, c;
 /* Look for any elements with the class "custom-select": */
